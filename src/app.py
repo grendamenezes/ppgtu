ad6e2fcf0e1ca8417ec6643fee4d94323a5b0d41
Grenda07 <126720472+Grenda07@users.noreply.github.com>
2023-03-01 20:55:36 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -169,7 +169,7 @@ def preenche_modelo(mes,ano,nome,df): #ex: 1,Presencial
 			ws2[cell.coordinate].value = cell.value
 	new_file_name = 'relatorio_'+month_name+'.xlsx'
 	wb.save(new_file_name)
-	return df
+	return new_file_name
 
 def download_zip(mes,ano,nome,df):
 	locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')
